08391b7317afaf693ba19e5d1f45cd15f2614ef6
Alireza <real7alireza@gmail.com>
2021-06-25 13:16:35 +0430

    use static files

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,5 +1,10 @@
 const Courses = () => {
-  return <h1>Courses page</h1>;
+  return (
+    <>
+      <h1>Courses page</h1>
+      <img src='/images/contactus.jpg' />
+    </>
+  );
 };
 
 export default Courses;
