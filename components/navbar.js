562e5a5fd24270aee493715dbfd6fb98bf4da2a7
Alireza <real7alireza@gmail.com>
2021-06-25 13:25:54 +0430

    css in js

--- components/navbar.js
+++ components/navbar.js
@@ -2,18 +2,34 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/courses">
-          <a>Courses</a>
-        </Link>
-      </li>
-    </ul>
+    <>
+      <style jsx>
+        {`
+          ul {
+            display: flex;
+          }
+          ul li {
+            list-style: none;
+            padding: 10px;
+          }
+          ul li a {
+            color: #aaa;
+          }
+        `}
+      </style>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/courses">
+            <a>Courses</a>
+          </Link>
+        </li>
+      </ul>
+    </>
   );
 };
 
