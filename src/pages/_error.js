b9843348d528c32fe31199458a9a546290e25ca8
Alireza <real7alireza@gmail.com>
2021-06-25 15:11:07 +0430

    customize error

---
+++ src/pages/_error.js
@@ -0,0 +1,5 @@
+const Error = () => {
+    return <h1>internal server error</h1>
+}
+
+export default Error
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 import Link from "next/link";
+import Error from 'next/error'
 
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -12,13 +13,21 @@ import Posts from "./../components/posts";
 import Navbar from "./../components/navbar";
 
 function Home(props) {
-  // console.log(props);
+
+  console.log(props.errorCode);
   // const [posts, setPosts] = useState([]);
   // useEffect(() => {
   //   axios
   //     .get("https://jsonplaceholder.typicode.com/posts")
   //     .then((res) => setPosts(res.data));
   // }, []);
+
+
+  if(props.errorCode){
+    // return <Error statusCode={props.errorCode} />
+    return <h1>erorrrrrrrrrrrrrrrrrrrrrrrrrrr</h1>
+  }
+
 
   return (
     <div>
@@ -34,12 +43,14 @@ function Home(props) {
 }
 
 export async function getServerSideProps() {
-  let res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  let res = await axios.get("https://jsonplaceholder.typicode.com/post");
+  let errorCode = res.status===200 ? false : res.status;
   let posts = await res.data;
 
   return {
     props: {
       posts,
+      errorCode
     },
   };
 }
